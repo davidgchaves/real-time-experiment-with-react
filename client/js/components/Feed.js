0821aa9136b33f410810e0e3e51a7192dc092de5
David G Chaves <davidgchaves@gmail.com>
2015-04-13 15:21:11 +0200

    Rename 'key' to 'id' to avoid name collision with React's 'key'

--- client/js/components/Feed.js
+++ client/js/components/Feed.js
@@ -10,9 +10,9 @@ var Feed = React.createClass({
 
   getInitialState: function() {
     var FEED_ITEMS = [
-      { key:'1', title: "Bergman's Persona", description: 'A young nurse, Alma, is put in charge of Elisabeth Vogler.', votes: 1966 },
-      { key:'2', title: "Kurosawa's Rashomon", description: 'A heinous crime and its aftermath are recalled from differing points of view.', votes: 1950 },
-      { key:'3', title: "Ozu's Bashun", description: 'Setsuko Hara and Jun Usami ride bicycles to the beach... and the vase, of course.', votes: 1949 },
+      { id:'1', title: "Bergman's Persona", description: 'A young nurse, Alma, is put in charge of Elisabeth Vogler.', votes: 1966 },
+      { id:'2', title: "Kurosawa's Rashomon", description: 'A heinous crime and its aftermath are recalled from differing points of view.', votes: 1950 },
+      { id:'3', title: "Ozu's Bashun", description: 'Setsuko Hara and Jun Usami ride bicycles to the beach... and the vase, of course.', votes: 1949 },
     ];
     return {
       items: FEED_ITEMS,
